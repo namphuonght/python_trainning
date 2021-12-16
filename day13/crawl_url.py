572709b32595e17354a56b50c727108d0e153465
namphuonght <trannamphuong2k@gmail.com>
2021-12-16 16:26:34 +0700

    update1

--- day13/crawl_url.py
+++ day13/crawl_url.py
@@ -21,6 +21,7 @@ import requests
 import logging
 import string
 import re
+import datetime
 
 
 logging.basicConfig(
@@ -41,7 +42,28 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', '-u', type=str,
             help='The URL want to crawl')
+    parser.add_argument('--start_page', '-s', type=int,
+            help='Start page', required=False)
+    parser.add_argument('--end_page', '-e', type=int,
+            help='End page', required=False)
+    parser.add_argument('--start_day', '-sd', type=str,
+            help='Start day', required=False)
+    parser.add_argument('--end_day', '-ed', type=str,
+            help='End day', required=False)
     return parser.parse_args()
+
+
+# =============================================================================
+
+
+def cvt_datetime_to_timestamp(time):
+    """
+    Function to convert from datetime to timestamp
+    """
+    date = datetime.datetime.strptime(time, f"%d/%m/%Y")
+    timestamp = datetime.datetime.timestamp(date)
+
+    return timestamp
 
 
 # =============================================================================
@@ -64,10 +86,13 @@ def crawler(url):
 
 
 def save_file(args):
-    urls = crawler(args.url)  
-    for url in urls:
-        f = open('list_url.txt', 'a', encoding='utf-8')
-        f.write(str(url)+'\n')
+    if args.start_page and args.end_page:
+        for n in range(args.start_page, args.end_page+1):
+            urls = crawler(args.url+f'-p{n}')  
+            for url in urls:
+                f = open('list_url.txt', 'a', encoding='utf-8')
+                f.write(str(url)+'\n')
+
     f.close
 
 
