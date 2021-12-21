d74a815de87b5b9d63955540163a82654b11be6c
namphuonght <trannamphuong2k@gmail.com>
2021-12-21 12:04:36 +0700

    update

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,10 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        
-        if x[0] == "-":
-            return False
-        elif x == x[::-1]:
+        if x == x[::-1]:
             return True
-        else:
-            return False
+        
+        
+        
--- plus_one.py
+++ plus_one.py
@@ -1,11 +1,7 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         strNum = ''
-        result = []
         for n in digits:
             strNum += str(n)
-        sum_one = str(int(strNum) + 1)
-        for s in sum_one:
-            result.append(s)
-            
-        return result
+        return [s for s in str(int(strNum)+1)]
+        
