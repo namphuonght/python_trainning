2e5d231a863cad9ea6bd6d87e4485bc094e9a0d6
namphuonght <trannamphuong2k@gmail.com>
2021-12-21 15:29:31 +0700

    update2

--- plus_one.py
+++ plus_one.py
@@ -1,7 +1,3 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        strNum = ''
-        for n in digits:
-            strNum += str(n)
-        return [s for s in str(int(strNum)+1)]
-        
+        return [n for n in str(int("".join(map(str, digits))) + 1)]
